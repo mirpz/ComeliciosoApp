a6749920182f238647516390ba4dc5aa2e38ebe6
mirpz <106282679+mirpz@users.noreply.github.com>
2022-06-15 18:49:07 -0500

    COM-74
    Main listo

--- app/src/main/java/com/example/comelicioso/MainActivity.java
+++ app/src/main/java/com/example/comelicioso/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ESTA PARTE ES EL MAIN
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
